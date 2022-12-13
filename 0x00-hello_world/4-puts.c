fdb525fc769cf810073983a8cf67e8d2862d4e37
odiedo <odiedopaul@gmail.com>
2022-12-13 11:13:53 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry poit
- *
- * Return: Always 0 (success)
+ *  main - Entry poit
+ *return: Always 0
  */
-
 int main(void) 
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
